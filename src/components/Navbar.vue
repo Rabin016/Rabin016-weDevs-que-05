8492a1074d5958d21b4ccc0e7ededf336f4c8f69
Rabin016 <rabinchakraborty0174@gmail.com>
2020-09-15 11:48:32 +0600

    minor fix

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
     <header
         class="text-gray-200 bg-indigo-700 rounded-b-xl py-5 px-8 w-10/12 md:w-8/12 shadow-lg mx-auto mb-5"
     >
-        <router-link to="/" class="px-8">
+        <router-link to="/" class="px-8 no-underline text-white">
             <h1 class="text-xl font-bold text-center">Welcome to Demo Post</h1>
         </router-link>
     </header>
